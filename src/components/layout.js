42def06640b71da620c5071f0c845ca9d2b953f2
graemetg <graemetg@gmail.com>
2020-08-30 22:40:50 +0100

    testing this change

--- src/components/layout.js
+++ src/components/layout.js
@@ -63,10 +63,10 @@ const TemplateWrapper = ({ children }) => {
               />
               <ul className="sidebar__menu">
                 <li>
-                  <Link to="/">Home</Link>
+                  <Link to="/">Homer</Link>
                 </li>
                 <li>
-                  <Link to="/about">About</Link>
+                  <Link to="/about">Abouter</Link>
                 </li>
               </ul>
               <p className="sidebar__social">
